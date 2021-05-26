decf4f09f531f64b78d7d1a2d2d82c1bd01b5027
Alisher Battalov <abattalov14@gmail.com>
2021-05-26 01:49:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board,index)
-  if board[index] = "X" || "O"
-    return true
-  elsif board[index] = " " || ""
+  if board[index] = " " || ""
     return false
+  elsif board[index] = "X" || "O"
+    return true
   end
 end
