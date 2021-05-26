ce007f3c83947fa88da026928c9bde837e42fff7
Alisher Battalov <abattalov14@gmail.com>
2021-05-26 02:16:13 +0000

    Automatically backed up by Learn

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -37,7 +37,7 @@ describe '#position_taken? in lib/position_taken.rb' do
 
     index = 0
     expect(position_taken?(board, index)).to be(true)
-    print board[0]
+
     index = 8
     expect(position_taken?(board, index)).to be(true)
   end
