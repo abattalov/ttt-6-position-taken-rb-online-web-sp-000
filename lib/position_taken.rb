a36bfc5ec628377b062dbc7914f70d004a3ed3d7
Alisher Battalov <abattalov14@gmail.com>
2021-05-26 01:42:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def position_taken?
+  if board[index] = " " || ""
+    return true
+  else
+    return false
+  end
+end
